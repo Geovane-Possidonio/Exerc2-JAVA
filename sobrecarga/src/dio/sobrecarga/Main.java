a113dce4c318ad4d4dbc72651daeffc6ca2c8598
Geovane-Possidonio <g.possidonio01@gmail.com>
2022-05-12 13:36:52 -0300

    Exercicios

---
+++ sobrecarga/src/dio/sobrecarga/Main.java
@@ -0,0 +1,12 @@
+package dio.sobrecarga;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Exercício Área
+        Quadrilatero.area(3);
+        Quadrilatero.area(5d,5d);
+        Quadrilatero.area(7,8,9);
+        Quadrilatero.area(5f,5f);
+    }
+}
